c7752e983eeab60e8a3e59c1fe781902ea1c5172
Joey Zhou <josephyzhou@gmail.com>
2013-12-12 18:36:05 -0800

    first button functional

--- JZProgressButton/JZProgressButton/JZViewController.m
+++ JZProgressButton/JZProgressButton/JZViewController.m
@@ -1,15 +1,17 @@
 //
 //  JZViewController.m
-//  JZProgressButton
+//  JZButtons
 //
 //  Created by Joseph Zhou on 12/12/13.
 //  Copyright (c) 2013 JoeyZhou. All rights reserved.
 //
 
 #import "JZViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface JZViewController ()
-
+@property (strong, nonatomic) IBOutlet UIButton *progressiveButton;
+@property (nonatomic) CALayer *progressLayer;
 @end
 
 @implementation JZViewController
@@ -17,13 +19,66 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // view stuff
+    self.view.backgroundColor = [UIColor colorWithRed:255/255.0f green:233/255.0f blue:165/255.0f alpha:1.0f];
+    
+    CALayer *layer = [self.progressiveButton layer];
+    [layer setBorderColor:[UIColor orangeColor].CGColor];
+    [layer setBorderWidth:0.3f];
+    
+    self.progressLayer = [CALayer layer];
+    self.progressLayer.delegate = self;
+    self.progressLayer.backgroundColor = [UIColor orangeColor].CGColor;
+    self.progressLayer.frame = CGRectMake(0, 0, 0, [self.progressiveButton layer].frame.size.height);
+    [layer addSublayer:self.progressLayer];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)progress1:(id)sender {
+    [self.progressiveButton setTitle:@"Uploading..." forState:UIControlStateNormal];
+    [UIView animateWithDuration:2.0 delay:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.progressLayer.frame = CGRectMake(0,
+                                              0,
+                                              [self.progressiveButton layer].frame.size.width,
+                                              self.progressLayer.frame.size.height);
+    }
+                     completion:^(BOOL finished) {
+                         [self.progressiveButton setTitle:@"Uploaded✓" forState:UIControlStateNormal];
+                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.8 * NSEC_PER_SEC), dispatch_get_current_queue(), ^{
+                             [self.progressiveButton setTitle:@"Upload" forState:UIControlStateNormal];
+                             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.8 * NSEC_PER_SEC), dispatch_get_current_queue(), ^{
+                                 
+                                 self.progressLayer.frame = CGRectMake(0,0,0,self.progressLayer.frame.size.height);
+                             });
+                         });
+                     }];
+    
+}
+
+- (IBAction)progress2:(id)sender {
+    [self.progressiveButton setTitle:@"Uploading..." forState:UIControlStateNormal];
+    [UIView animateWithDuration:2.0 delay:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.progressLayer.frame = CGRectMake(0,
+                                              0,
+                                              [self.progressiveButton layer].frame.size.width,
+                                              self.progressLayer.frame.size.height);
+    }
+                     completion:^(BOOL finished) {
+                         [self.progressiveButton setTitle:@"Uploaded✓" forState:UIControlStateNormal];
+                         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.8 * NSEC_PER_SEC), dispatch_get_current_queue(), ^{
+                             [self.progressiveButton setTitle:@"Upload" forState:UIControlStateNormal];
+                             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.8 * NSEC_PER_SEC), dispatch_get_current_queue(), ^{
+                                 
+                                 self.progressLayer.frame = CGRectMake(0,0,0,self.progressLayer.frame.size.height);
+                             });
+                         });
+                     }];
+    
 }
 
 @end
